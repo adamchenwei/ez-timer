04ad1b92caf82682940058f42b7b2436ff69b50b
Adam Chen Wei <adamchenwei@gmail.com>
2018-09-14 22:11:09 -0400

    build(storybook): but not working, always error

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,29 @@
+// const path = require("path");
+
+// module.exports = (baseConfig, env, defaultConfig) => {
+//   defaultConfig.resolve.alias = {
+//     ...defaultConfig.resolve.alias,
+//     "@": path.resolve(__dirname, "../src"),
+//   };
+
+//   return defaultConfig;
+// };
+
+const path = require("path");
+
+// Export a function. Accept the base config as the only param.
+module.exports = (storybookBaseConfig, configType) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  storybookBaseConfig.module.rules.push({
+    test: /\.scss$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../src")
+  });
+
+  // Return the altered config
+  return storybookBaseConfig;
+};
